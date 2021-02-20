2db149435265f57dcee13b6066d51b00435c0ceb
Lucky <lqlucky1106@qq.com>
2021-02-20 17:36:54 +0800

    完成登录静态页面的渲染

---
+++ src/plugins/element.js
@@ -0,0 +1,59 @@
+import Vue from 'vue'
+import {
+  Pagination,
+  Dialog,
+  Menu,
+  MenuItem,
+  Input,
+  Radio,
+  RadioGroup,
+  Checkbox,
+  Select,
+  Option,
+  Button,
+  Table,
+  TableColumn,
+  DatePicker,
+  Form,
+  FormItem,
+  Row,
+  Col,
+  Upload,
+  Card,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Link,
+  MessageBox,
+  Message
+} from 'element-ui'
+
+Vue.use(Pagination)
+Vue.use(Dialog)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(Input)
+Vue.use(Radio)
+Vue.use(RadioGroup)
+Vue.use(Checkbox)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(Button)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(DatePicker)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Upload)
+Vue.use(Card)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Link)
+
+Vue.prototype.$confirm = MessageBox.confirm
+Vue.prototype.$message = Message
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,6 +1,59 @@
 <template>
   <div class="login">
-    <div class="loginbox"></div>
+    <div class="loginbox">
+      <div class="loginbox_title">
+        <img src="@/assets/imgs/layout_icon.png" alt="" />
+        <span>黑马面面</span>
+        <span>|</span>
+        <span>用户登录</span>
+      </div>
+      <el-form
+        :model="loginForm"
+        :rules="rules"
+        ref="loginRefForm"
+        class="demo-ruleForm"
+      >
+        <el-form-item prop="phone">
+          <el-input
+            v-model="loginForm.phone"
+            placeholder="请输入手机号"
+            prefix-icon="el-icon-user"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginForm.password"
+            placeholder="请输入密码"
+            prefix-icon="el-icon-lock"
+          ></el-input>
+        </el-form-item>
+        <el-row>
+          <el-col :span="18">
+            <el-form-item prop="code">
+              <el-input
+                v-model="loginForm.code"
+                placeholder="请输入验证码"
+                prefix-icon="el-icon-key"
+              ></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6"
+            ><img src="http://159.75.11.233/heimamm/public/captcha?type=login" alt="" class="captcha" width="100%" height="40px"
+          /></el-col>
+        </el-row>
+        <el-form-item>
+          <el-checkbox-group v-model="loginForm.checked" >
+            <el-checkbox>我已阅读并同意<el-link type="primary" :underline="false" href="https://www.showdoc.com.cn/538567623707717">用户协议</el-link>和<el-link type="primary" :underline="false" href="https://element.eleme.cn/#/zh-CN">隐私条款</el-link></el-checkbox>
+          </el-checkbox-group>
+        </el-form-item>
+          <el-button type="primary" class="loginbtn">登录</el-button>
+        <el-form-item>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="success" class="loginbtn">注册</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
     <div class="loginimg">
       <a href="javascript:;"><img src="@/assets/imgs/login_bg.png" alt=""/></a>
     </div>
@@ -11,7 +64,15 @@
 export default {
   mounted () {},
   data () {
-    return {}
+    return {
+      loginForm: {
+        phone: '',
+        password: '',
+        code: '',
+        checked: false
+      },
+      rules: {}
+    }
   },
   methods: {}
 }
@@ -29,12 +90,26 @@ export default {
   .loginbox {
     width: 478px;
     height: 550px;
+    padding: 42px;
+    border-radius: 10px;
     background: #f5f5f5;
+    .loginbox_title {
+      display: flex;
+      align-items: center;
+      height: 30px;
+      margin-bottom: 22px;
+      span {
+        margin-left: 10px;
+        &:nth-child(2) {
+          font-size: 20px;
+        }
+      }
+    }
   }
   .loginimg {
     width: 633px;
     height: 435px;
-    animation: myfirst 1s;
+    animation: myfirst 1.5s;
     -webkit-animation: myfirst 1s; /* Safari and Chrome */
   }
   @keyframes myfirst {
@@ -54,5 +129,16 @@ export default {
       transform: translateY(0px);
     }
   }
+}
+.captcha {
+  width: 100%;
+  height: 40px;
+  border: 1px dotted #ccc
+}
+.el-link {
+  vertical-align: baseline;
+}
+.loginbtn {
+  width: 100%;
 }
 </style>
